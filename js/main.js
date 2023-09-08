f146fc985195a9e6077b40309274788ae63a13e4
zim <49727508+Zimehx@users.noreply.github.com>
2023-09-09 01:51:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7150,7 +7150,7 @@
                         staticClass: "player-info"
                     }, [s("div", {
                         style: {
-                            color: e.perk_color_picked
+                            color: e.nameColor
                         },
                         attrs: {
                             id: "account-name"
@@ -7261,7 +7261,7 @@
                         },
                         getAvatarUrl: (e, t) => t ? "https://cdn.discordapp.com/avatars/" + e + "/" + t + ".png" : "https://cdn.discordapp.com/embed/avatars/0.png",
                         setAccountData(e) {
-                            e.permissions && (window.gameObj = e8), GAME.account = e, this.account = e, this.avatarUrl = this.getAvatarUrl(e.discord_id, e.discord_avatar), this.name = e.locked_name || e.discord_name, this.nameColor = e.perk_color_picked ? "#" + e.perk_color_picked : "#ffffff", e8.ownUid = e.uid
+                            e.permissions && (window.gameObj = e8), GAME.account = e, this.account = e, this.avatarUrl = this.getAvatarUrl(e.discord_id, e.discord_avatar), this.name = e.perk_name_picked || e.discord_name, this.nameColor = e.perk_color_picked ? "#" + e.perk_color_picked : "#ffffff", e8.ownUid = e.uid
                         },
                         onXpUpdate(e) {
                             if (this.account) {
